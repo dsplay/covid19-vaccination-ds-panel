b6c991363987ab0302195f460e6d4647d6f6fbe2
José Guilherme <jgnvs6@gmail.com>
2021-05-07 10:20:05 -0300

    fix return getInfoCountries (add fields and return list with all vacinnateds)

--- src/utils/getInfoCountries.js
+++ src/utils/getInfoCountries.js
@@ -17,17 +17,41 @@ function filterCountries(queryCountries) {
 
 function GroupCountryByCode(json) {
   const counstriesMap = new Map();
+  const counstriesValidData = {};
+
   json.forEach((line) => {
-    if (line.people_vaccinated !== '0' && line.people_vaccinated !== '0') {
-      counstriesMap.set(line.iso_code, {
-        location: line.location,
+    if (!counstriesValidData[line.iso_code]) counstriesValidData[line.iso_code] = [];
+    if (line.people_vaccinated !== '0' && line.people_vaccinated !== '') {
+      counstriesValidData[line.iso_code].push({
         date: line.date,
         people_vaccinated: line.people_vaccinated,
       });
     }
+
+    if (line.people_vaccinated !== '0' && line.people_vaccinated !== '') {
+      counstriesMap.set(line.iso_code, {
+        location: line.location,
+        date: line.date,
+        people_vaccinated: line.people_vaccinated,
+        people_fully_vaccinated: line.people_fully_vaccinated,
+        total_vaccinations: line.total_vaccinations,
+      });
+    }
   });
 
-  return Object.fromEntries(counstriesMap);
+  const countries = {};
+  counstriesMap.forEach((value, key) => {
+    countries[key] = {
+      location: value.location,
+      date: value.date,
+      people_vaccinated: value.people_vaccinated,
+      people_vaccinated_history: counstriesValidData[key],
+      people_fully_vaccinated: value.people_fully_vaccinated,
+      total_vaccinations: value.total_vaccinations,
+    };
+  });
+
+  return countries;
 }
 
 export default function getInfoCountries(queryCountries) {
@@ -39,6 +63,9 @@ export default function getInfoCountries(queryCountries) {
       country.location = countriesOrg[country.code].location;
       country.people_vaccinated = countriesOrg[country.code].people_vaccinated;
       country.date = countriesOrg[country.code].date;
+      country.people_vaccinated_history = countriesOrg[country.code].people_vaccinated_history;
+      country.people_fully_vaccinated = countriesOrg[country.code].people_fully_vaccinated;
+      country.total_vaccinations = countriesOrg[country.code].total_vaccinations;
     });
 
     return countries;
