3b3ff377d8a92ab58320b020c81c98da6444f260
José Guilherme <jgnvs6@gmail.com>
2021-05-11 14:11:52 -0300

    remove fixed decimal if is 0

--- src/utils/NumberFormatter.js
+++ src/utils/NumberFormatter.js
@@ -1,17 +1,17 @@
 function kFormatter(number) {
-  return `${(number / 1e3).toFixed(1)}k`;
+  return `${(number / 1e3).toFixed(1).replace('.0', '')}k`;
 }
 
 function mFormatter(number) {
-  return `${(number / 1e6).toFixed(1)}M`;
+  return `${(number / 1e6).toFixed(1).replace('.0', '')}M`;
 }
 
 function bFormatter(number) {
-  return `${(number / 1e6).toFixed(1)}B`;
+  return `${(number / 1e6).toFixed(1).replace('.0', '')}B`;
 }
 
 export default function NumberFormatter(number) {
-  if (number > 999) return kFormatter(number);
-  if (number > 999999) return mFormatter(number);
   if (number > 999999999) return bFormatter(number);
+  if (number > 999999) return mFormatter(number);
+  if (number < 999) return kFormatter(number);
 }
