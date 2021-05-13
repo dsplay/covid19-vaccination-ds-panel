8769824e9d0ea4e3726a34550fee982a1ac0a882
José Guilherme <jgnvs6@gmail.com>
2021-05-13 09:12:12 -0300

    remove console.log and add default string in Line component

--- src/components/Table/Line.jsx
+++ src/components/Table/Line.jsx
@@ -2,23 +2,22 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function Line({ country }) {
-  console.log(country);
   return (
     <tr>
       <td>
         { `${country.flag} ${country.location}` }
       </td>
       <td>
-        { country.population }
+        { country.population || '-'}
       </td>
       <td>
-        { country.total_vaccinations }
+        { country.total_vaccinations || '-' }
       </td>
       <td>
-        { country.people_vaccinated }
+        { country.people_vaccinated || '-' }
       </td>
       <td>
-        { country.people_fully_vaccinated }
+        { country.people_fully_vaccinated || '-' }
       </td>
     </tr>
   );
