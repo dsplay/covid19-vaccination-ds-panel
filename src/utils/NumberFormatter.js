8b24a1e97b613395167d1df8de4a8add37982d94
José Guilherme <jgnvs6@gmail.com>
2021-05-18 08:26:57 -0300

    refactor numbreformatter function

--- src/utils/NumberFormatter.js
+++ src/utils/NumberFormatter.js
@@ -1,17 +1,10 @@
-function kFormatter(number) {
-  return `${(number / 1e3).toFixed(1).replace('.0', '')}k`;
+function generateNumberFormat(number, divisor, measure) {
+  return `${(number / divisor).toFixed(1).replace('.0', '')}${measure}`;
 }
 
-function mFormatter(number) {
-  return `${(number / 1e6).toFixed(1).replace('.0', '')}M`;
-}
-
-function bFormatter(number) {
-  return `${(number / 1e9).toFixed(1).replace('.0', '')}B`;
-}
-
-export default function NumberFormatter(number) {
-  if (number > 999999999) return bFormatter(number);
-  if (number > 999999) return mFormatter(number);
-  if (number > 999) return kFormatter(number);
+export default function numberFormatter(number) {
+  if (number > 999999999) return generateNumberFormat(number, 1e9, 'B');
+  if (number > 999999) return generateNumberFormat(number, 1e6, 'M');
+  if (number > 999) return generateNumberFormat(number, 1e3, 'k');
+  return number;
 }
