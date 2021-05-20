889f2127ba58996a8d2f99d2718d178ec270abec
José Guilherme <jgnvs6@gmail.com>
2021-05-20 09:49:59 -0300

    logic to display world info first in table

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -5,10 +5,14 @@ import { useTranslation } from 'react-i18next';
 import Line from './Line';
 import useInterval from '../../utils/useInterval';
 
+import './Table.sass';
+
 function Table({ countries = [] }) {
   const { t } = useTranslation();
 
-  const ELEMENTS_BY_PAGE = 3;
+  const worldInfoIndex = countries.findIndex((country) => country.code === 'World');
+
+  const ELEMENTS_BY_PAGE = (worldInfoIndex < 0) ? 3 : 2;
   const NUMBER_OF_PAGES = Math.ceil(countries.length / ELEMENTS_BY_PAGE);
 
   const [currentPage, setCurrentPage] = useState(0);
@@ -17,26 +21,39 @@ function Table({ countries = [] }) {
     setCurrentPage((currentPage + 1) % NUMBER_OF_PAGES);
   }, countries.length > 1 ? 15000 : null);
 
+  let lineWorld;
+  const countriesInfo = [...countries];
+
+  if (!(worldInfoIndex < 0)) {
+    lineWorld = <Line key="World" country={countries[worldInfoIndex]} />;
+    countriesInfo.splice(worldInfoIndex, 1);
+  }
+
   const lines = [];
   let position;
   for (let i = 0; i < ELEMENTS_BY_PAGE; i += 1) {
     position = (currentPage * ELEMENTS_BY_PAGE) + i;
-    if (position < countries.length) {
-      lines.push(<Line key={i} country={countries[position]} />);
+    if (position < countriesInfo.length && countriesInfo[position].code !== 'World') {
+      lines.push(<Line key={i} country={countriesInfo[position]} />);
     }
   }
 
   return (
     <>
       <table>
-        <tr>
-          <th>{t('Country')}</th>
-          <th>{t('Population')}</th>
-          <th>{t('Doses Given')}</th>
-          <th>{t('% at least 1 dose')}</th>
-          <th>{t('% fully vaccinated')}</th>
-        </tr>
-        { lines }
+        <thead>
+          <tr>
+            <th>{t('Country')}</th>
+            <th>{t('Population')}</th>
+            <th>{t('Doses Given')}</th>
+            <th>{t('% at least 1 dose')}</th>
+            <th>{t('% fully vaccinated')}</th>
+          </tr>
+        </thead>
+        <tbody>
+          { lineWorld }
+          { lines }
+        </tbody>
       </table>
     </>
   );
