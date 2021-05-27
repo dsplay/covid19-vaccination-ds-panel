143851c902740ad759370584a6baf8fa829413be
José Guilherme <jgnvs6@gmail.com>
2021-05-27 10:07:26 -0300

    add google analytics and add react-helmet to modify website title

--- src/Pages/MainPage.jsx
+++ src/Pages/MainPage.jsx
@@ -2,10 +2,14 @@ import React from 'react';
 import QRCode from 'qrcode.react';
 import PropTypes from 'prop-types';
 import { useTranslation } from 'react-i18next';
-import i18n from '../i18n';
-import ObjectValidateCountry from '../utils/ObjectValidateCountry';
 
+import { Helmet } from 'react-helmet';
+
+import i18n from '../i18n';
+
+import ObjectValidateCountry from '../utils/ObjectValidateCountry';
 import { translateManyNameCountries } from '../utils/countriesNameTranslate';
+
 import Graph from '../components/Graph/Graph';
 import CardList from '../components/CardList/CardList';
 import Table from '../components/Table/Table';
@@ -18,6 +22,11 @@ function MainPage({ selectedCountry = {}, countries = [] }) {
 
   return (
     <>
+      <Helmet>
+        <title>
+          {t('Covid-19 Vaccination')}
+        </title>
+      </Helmet>
       <div className="flex wrapper-title">
         <h1>{t('Covid-19 Vaccination')}</h1>
       </div>
